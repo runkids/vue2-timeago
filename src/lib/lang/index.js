19da66d04df58cec1973f12c036db52f71da445f
Fernando Romano <fernando@ralnet.com.br>
2019-02-28 15:12:35 -0300

    Add Portuguese-Brazil

--- src/lib/lang/index.js
+++ src/lib/lang/index.js
@@ -2,6 +2,7 @@ import zh_TW from './zh_TW';
 import zh_CN from './zh_CN';
 import en from './en';
 import jp from './jp';
+import pt_BR from './pt_BR';
 
 
 export default{
@@ -9,4 +10,5 @@ export default{
   zh_CN,
   en,
   jp,
+  pt_BR
 }
