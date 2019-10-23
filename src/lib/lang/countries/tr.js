4b46d013c6870ac1c180b9c4015a5b1fd9adc91a
Ali Emre Taşkın <12280383+aliemretaskin@users.noreply.github.com>
2019-10-23 03:37:41 +0300

    Create tr.js
    Turkish translation

---
+++ src/lib/lang/countries/tr.js
@@ -0,0 +1,16 @@
+export default {
+  short: {
+    now: 'şimdi',
+    sec:'sn',
+    min: 'dk',
+    hour: 's',
+    day: 'g',
+  },
+  long: {
+    now: 'şimdi',
+    sec: 'saniye',
+    min: 'dakika',
+    hour: 'saat',
+    day: 'gün',
+  }
+}
