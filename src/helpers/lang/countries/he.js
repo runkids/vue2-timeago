91c06f62b0ca734b6c47c04d4f7ed486daec4033
harshgour <harshgour57@gmail.com>
2021-10-13 02:33:21 +0530

    hebrew support

---
+++ src/helpers/lang/countries/he.js
@@ -0,0 +1,20 @@
+function formatToLongString(time, type) {
+  return `לִפנֵי ${type} ${time}`
+}
+
+export default {
+  short: {
+    now: 'זֶה עַתָה',
+    sec: 'שניות',
+    min: 'דַקָה',
+    hour: 'שָׁעָה',
+    day: 'יְוֹם',
+  },
+  long: {
+    now: 'זֶה עַתָה',
+    sec: (time) => formatToLongString(time, 'שנייה'),
+    min: (time) => formatToLongString(time, 'דַקָה'),
+    hour: (time) => formatToLongString(time, 'שָׁעָה'),
+    day: (time) => formatToLongString(time, 'יְוֹם'),
+  }
+}
